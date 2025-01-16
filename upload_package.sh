dd3dc7408d2b442a87b3b1215fde1ae23b2b5b7b
zackees <z@zackees.com>
2025-01-16 00:06:03 -0800

    chore: update project name and dependencies in configuration

--- upload_package.sh
+++ upload_package.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 rm -rf build dist
-. ./activate.sh
-pip install wheel twine
-echo "Building Source and Wheel (universal) distribution…"
-python setup.py sdist bdist_wheel --universal
+uv build
 echo "Uploading the package to PyPI via Twine…"
+uv add twine
 twine upload dist/* --verbose
 # echo Pushing git tags…
